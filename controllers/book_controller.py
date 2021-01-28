f4bad3f76119dc59636c008c07c69e7ce3343f33
KathleenEdie <kathleenedie@gmail.com>
2021-01-28 08:52:57 +0000

    edit route added

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -31,11 +31,29 @@ def new_book():
 
 @books_blueprint.route('/books', methods=['POST'])
 def create_book():
+    first_name = request.form['author_namef']
+    last_name = request.form['author_namel']
+    author_new = Author(first_name, last_name)
+    author = author_repository.save(author_new)
     title = request.form['title']
     genre = request.form['genre']
     publisher = request.form['publisher']
-    author_name = request.form['author_id']
-    author = author_repository.select(author_id)
     book = Book(title, genre, publisher, author, id)
     book_repository.save(book)
+    return redirect('/books')
+
+@books_blueprint.route("/books/<id>/edit", methods=["GET"])
+def edit_book(id):
+    book = book_repository.select(id)
+    author = author_repository.select_all()
+    return render_template('books/edit.html', book = book, author = author)
+
+@books_blueprint.route("/books/<id>", methods=["POST"])
+def update_book(id):    
+    title = request.form['title']
+    author_id = request.form['author_id']
+    genre = request.form['genre']
+    publisher = request.form['publisher']
+    author = author_repository.select(author_id)
+    book = Book(title, publisher, genre, author, id)
     return redirect('/books')
